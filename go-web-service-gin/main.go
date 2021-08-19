35d48915c747f56b5440f5da274fe3316e0ee119
mandarinkb <mandarinkb@gmail.com>
2021-08-20 02:39:10 +0700

    add go-web-service-gin

--- go-web-service-gin/main.go
+++ go-web-service-gin/main.go
@@ -1,110 +1,156 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
-// type user struct {
-// 	USER_ID  int    `json:"userId"`
-// 	USERNAME string `json:"username"`
-// 	PASSWORD string `json:"password"`
-// 	ROLE     string `json:"role"`
-// }
-
-// var users = []user{
-// 	{USER_ID: 1, USERNAME: "test", PASSWORD: "test-password", ROLE: "admin"},
-// 	{USER_ID: 2, USERNAME: "test1", PASSWORD: "test1-password", ROLE: "assistant"},
-// 	{USER_ID: 3, USERNAME: "test2", PASSWORD: "test2-password", ROLE: "admin"},
-// 	{USER_ID: 4, USERNAME: "test3", PASSWORD: "test3-password", ROLE: "assistant"},
-// }
-
-// // getUsers responds with the list of all users as JSON.
-// func getUsers(c *gin.Context) {
-// 	c.IndentedJSON(http.StatusOK, users)
-// }
-
-// // postUsers adds an user from JSON received in the request body.
-// func postUsers(c *gin.Context) {
-// 	var newUser user
-
-// 	// Call BindJSON to bind the received JSON to
-// 	// newUser.
-// 	err := c.BindJSON(&newUser)
-// 	if err != nil {
-// 		return
-// 	}
-
-// 	// Add the new user to the slice.
-// 	users = append(users, newUser)
-// 	c.IndentedJSON(http.StatusCreated, newUser)
-// }
-
-// // getUserByID locates the album whose ID value matches the id
-// // parameter sent by the client, then returns that album as a response.
-// func getUserByID(c *gin.Context) {
-// 	idStr := c.Param("id")
-// 	id, err := strconv.Atoi(idStr)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-
-// 	// Loop through the list of users, looking for
-// 	// an album whose ID value matches the parameter.
-// 	for _, a := range users {
-// 		if a.USER_ID == id {
-// 			c.IndentedJSON(http.StatusOK, a)
-// 			return
-// 		}
-// 	}
-// 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
-// }
-
-//
-
-/*****************************
-***  for without framwork  ***
-*****************************/
-
-type user struct {
+type User struct {
 	USER_ID  int    `json:"userId"`
 	USERNAME string `json:"username"`
 	PASSWORD string `json:"password"`
 	ROLE     string `json:"role"`
 }
 
-var users = []user{
+var users = []User{
 	{USER_ID: 1, USERNAME: "test", PASSWORD: "test-password", ROLE: "admin"},
-	{USER_ID: 2, USERNAME: "test1", PASSWORD: "test1-password", ROLE: "assistant"},
-	{USER_ID: 3, USERNAME: "test2", PASSWORD: "test2-password", ROLE: "admin"},
-	{USER_ID: 4, USERNAME: "test3", PASSWORD: "test3-password", ROLE: "assistant"},
+	{USER_ID: 2, USERNAME: "test2", PASSWORD: "test2-password", ROLE: "assistant"},
+	{USER_ID: 3, USERNAME: "test3", PASSWORD: "test3-password", ROLE: "admin"},
+	{USER_ID: 4, USERNAME: "test4", PASSWORD: "test4-password", ROLE: "assistant"},
 }
 
-type respose struct {
-	Message string `json:"message"`
+var user = User{USER_ID: 1, USERNAME: "test", PASSWORD: "test-password", ROLE: "admin"}
+
+func readUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users) // return ค่า http status และค่า json obj หรือ json array ออกให้เลย
 }
 
-func getAllUsers(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(users)
-}
-func homePage(w http.ResponseWriter, r *http.Request) {
-	res := respose{
-		Message: "Hwllo rest api",
+func readUserByID(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr) // แปลงเป็น int
+	if err != nil {
+		fmt.Println(err)
 	}
-	json.NewEncoder(w).Encode(res)
+	// ค้นหา id  ที่ตรงกับ mock data
+	for _, user := range users {
+		if user.USER_ID == id {
+			c.IndentedJSON(http.StatusOK, user)
+			return //ถ้าพบก็ออกจาก for loop ทันที
+		}
+	}
+	// กรณี id ที่ค้นหาไม่พบ
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"}) // gin.H{} สามารถใส่ json ได้เลย
 }
-func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/users", getAllUsers)
-	http.ListenAndServe(":8080", nil)
+
+func createUsers(c *gin.Context) {
+	var reqBody User
+	// request body จะถูกเรียกเมื่อ ใช้ ฟังก์ชัน BindJSON โดยค่าใน body จะตรงกับ reqBody
+	// reqBody ชื่อ filed ในนี้จะต้องตรงกับ ค่า key ใน body ด้วย
+	err := c.BindJSON(&reqBody) // แปลงค่าจาก body payload เป็น struct
+	if err != nil {
+		return
+	}
+	// Add the new user to the slice.
+	users = append(users, reqBody)
+	c.IndentedJSON(http.StatusCreated, reqBody)
 }
+
+func updateUsers(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr) // แปลงเป็น int
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var reqBody User
+	var newUser User
+	var sliceUser []User         // ไว้ อัพเดท slice User
+	err2 := c.BindJSON(&reqBody) // แปลงค่าจาก body payload เป็น struct
+	if err2 != nil {
+		return
+	}
+	// ตรวจเช็ค id ที่รับเข้ามาก่อน
+	haveId := false
+	for _, user := range users {
+		if user.USER_ID == id {
+			haveId = true
+		}
+	}
+	// ค้นหา id  ที่ตรงกับ mock data
+	if haveId {
+		for i, user := range users {
+			if user.USER_ID == id {
+				newUser = User{
+					USER_ID:  users[i].USER_ID,
+					USERNAME: reqBody.USERNAME,
+					PASSWORD: reqBody.PASSWORD,
+					ROLE:     reqBody.ROLE,
+				}
+				sliceUser = append(sliceUser, newUser)
+
+			} else {
+				newUser = User{
+					USER_ID:  users[i].USER_ID,
+					USERNAME: users[i].USERNAME,
+					PASSWORD: users[i].PASSWORD,
+					ROLE:     users[i].ROLE,
+				}
+				sliceUser = append(sliceUser, newUser)
+			}
+		}
+		users = sliceUser
+		c.IndentedJSON(http.StatusOK, reqBody)
+		return //ถ้าพบก็ออกจาก for loop ทันที
+	}
+	// กรณี id ที่ค้นหาไม่พบ
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
+func deleteUsers(c *gin.Context) {
+	idStr := c.Param("id")
+	id, err := strconv.Atoi(idStr) // แปลงเป็น int
+	if err != nil {
+		fmt.Println(err)
+	}
+	var newUser User
+	var sliceUser []User // ไว้ อัพเดท slice User
+	// ตรวจเช็ค id ที่รับเข้ามาก่อน
+	haveId := false
+	for _, user := range users {
+		if user.USER_ID == id {
+			haveId = true
+		}
+	}
+	// ค้นหา id  ที่ตรงกับ mock data
+	if haveId {
+		for i, user := range users {
+			if user.USER_ID != id {
+				newUser = User{
+					USER_ID:  users[i].USER_ID,
+					USERNAME: users[i].USERNAME,
+					PASSWORD: users[i].PASSWORD,
+					ROLE:     users[i].ROLE,
+				}
+				sliceUser = append(sliceUser, newUser)
+			}
+		}
+		users = sliceUser
+		c.IndentedJSON(http.StatusOK, gin.H{"message": "delete user success"})
+		return //ถ้าพบก็ออกจาก for loop ทันที
+	}
+	// กรณี id ที่ค้นหาไม่พบ
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
+}
+
 func main() {
-	// router := gin.Default()
-	// router.GET("/users", getUsers)
-	// router.GET("/users/:id", getUserByID)
-	// router.POST("/users", postUsers)
-	// router.Run("localhost:8080")
-	fmt.Println("service running at port 8080")
-	handleRequest()
+	router := gin.Default()
+	router.GET("/users", readUsers)
+	router.GET("/users/:id", readUserByID)
+	router.POST("/users", createUsers)
+	router.PUT("/users/:id", updateUsers)
+	router.DELETE("/users/:id", deleteUsers)
+	router.Run("localhost:8080")
 }
